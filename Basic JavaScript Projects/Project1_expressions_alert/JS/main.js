b7407583ac00e5df166001e8a97dc775e68f740a
tdel010 <tomdelaney.metta@gmail.com>
2026-03-17 10:53:03 -0700

    Add main.js with JavaScript examples and create text.txt for file structure verification

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+document.write("Let\'s concatenate" + " this sentence. "); //The backslash is used to escape the single quote in "Let's" so that it doesn't end the string prematurely. Also, the + operator is used to concatenate the two strings "Let's concatenate" and " this sentence. " together, resulting in "Let's concatenate this sentence. ".
+
+var X = "This is my super duper string!"; // Defining a variable
+window.alert(X); // This displays an alert box with the content of variable X
+
+var A = "I like "; // Defining a variable A with the string "I like "
+
+var B = "Chicken and " + "Waffles. "; // Defining a variable B as the concatenation of two strings "Chicken and " and "Waffles. "
+
+document.write(A + B); // This writes the content of variables to the document, which will display "I like Chicken and Waffles. " on the webpage.
+
+var Bikes = "Tom\'s Bikes", Trail = "Stumpjumper ", eMTB = "YT Decoy ", Road = "Trek Checkpoint "; // This line defines multiple variables: Bikes, Trail, eMTB, and Road, each assigned a string value. The backslash is used to escape the single quote in "Tom's" so that it doesn't end the string prematurely.
+document.write(Trail); // This writes the content of variable Trail to the document, which will display "Stumpjumper" on the webpage.
+
+document.write(2 + 2); // This writes the result of the expression 2 + 2 to the document, which will display "4" on the webpage. The + operator is used here to perform addition on the two numbers 2 and 2, resulting in 4.
+
+
+
